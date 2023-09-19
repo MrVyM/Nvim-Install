c1c48b09e453cddbe8de50af88948a84f275e5f5
vianney.marticou <la.regle.du.jeu.ytb@gmail.com>
2023-09-19 21:43:37 +0200

    [INSTALL] Add the directory in PATH

--- install.sh
+++ install.sh
@@ -16,5 +16,5 @@ done;
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
     ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-git -C config/nvim pull
+git -C config/nvim pull & 
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' &
